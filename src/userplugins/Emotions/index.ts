9e4f07f25b2eec15afa0452215f85332183caf7f
Shiro <bayuofficial773@gmail.com>
2024-05-09 09:57:39 +0700

    Simplified Emotions commands

--- src/userplugins/Emotions/index.ts
+++ src/userplugins/Emotions/index.ts
@@ -1,13 +1,77 @@
+import { ApplicationCommandOptionType, findOption } from "@api/Commands";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
 
-async function getImage(type: string): Promise<{ image: string, source: string; }> {
-    const res = await fetch(`https://nekos.best/api/v2/${type}`);
+async function getRandomGif(type: string, query?: string): Promise<string> {
+    const url = query ? `https://nekos.best/api/v2/search?query=${encodeURI(query)}&category=${encodeURI(type)}&type=2` : `https://nekos.best/api/v2/${encodeURI(type)}`;
+    const res = await fetch(url);
     const data = await res.json();
-    const { url: image } = data.results[0];
-    return image;
+    if (!data.results || data.results.length === 0) { throw new Error(`No GIF found for ${type} with query \`${query}\`.`); }
+    return data.results[0].url;
 }
+
+function createCommand(name: string, type: string, description: string) {
+    return {
+        name,
+        description,
+        options: [
+            {
+                name: "query",
+                description: "Get a specific gif",
+                type: ApplicationCommandOptionType.STRING
+            },
+        ],
+        async execute(opts: any) {
+            const query = findOption(opts, "query", "");
+            const imageUrl = await getRandomGif(type, query);
+            return { content: `[${name}](${imageUrl})` };
+        }
+    };
+}
+
+const commands = [
+    createCommand("lurk", "lurk", "Send random lurk gif."),
+    createCommand("shoot", "shoot", "Send random shoot gif."),
+    createCommand("sleep", "sleep", "Send random sleep gif."),
+    createCommand("shrug-gif", "shrug", "Send random shrug gif."),
+    createCommand("stare", "stare", "Send random stare gif."),
+    createCommand("wave", "wave", "Send random wave gif."),
+    createCommand("poke", "poke", "Send random poke gif."),
+    createCommand("smile", "smile", "Send random smile gif."),
+    createCommand("peck", "peck", "Send random peck gif."),
+    createCommand("wink", "wink", "Send random wink gif."),
+    createCommand("blush-gif", "blush", "Send random blush gif."),
+    createCommand("smug-gif", "smug", "Send random smug gif."),
+    createCommand("tickle", "tickle", "Send random tickle gif."),
+    createCommand("yeet", "yeet", "Send random yeet gif."),
+    createCommand("think", "think", "Send random think gif."),
+    createCommand("highfive", "highfive", "Send random highfive gif."),
+    createCommand("feed", "feed", "Send random feed gif."),
+    createCommand("bite", "bite", "Send random bite gif."),
+    createCommand("bored", "bored", "Send random bored gif."),
+    createCommand("nom", "nom", "Send random nom gif."),
+    createCommand("yawn", "yawn", "Send random yawn gif."),
+    createCommand("facepalm", "facepalm", "Send random facepalm gif."),
+    createCommand("cuddle", "cuddle", "Send random cuddle gif."),
+    createCommand("kick-gif", "kick", "Send random kick gif."),
+    createCommand("happy-gif", "happy", "Send random happy gif."),
+    createCommand("hug", "hug", "Send random hug gif."),
+    createCommand("baka", "baka", "Send random baka gif."),
+    createCommand("pat", "pat", "Send random pat gif."),
+    createCommand("nod", "nod", "Send random nod gif."),
+    createCommand("nope", "nope", "Send random nope gif."),
+    createCommand("kiss", "kiss", "Send random kiss gif."),
+    createCommand("dance", "dance", "Send random dance gif."),
+    createCommand("punch", "punch", "Send random punch gif."),
+    createCommand("handshake", "handshake", "Send random handshake gif."),
+    createCommand("slap", "slap", "Send random slap gif."),
+    createCommand("cry", "cry", "Send random cry gif."),
+    createCommand("pout", "pout", "Send random pout gif."),
+    createCommand("handhold", "handhold", "Send random handhold gif."),
+    createCommand("thumbsup", "thumbsup", "Send random thumbsup gif."),
+    createCommand("laugh", "laugh", "Send random laugh gif.")
+];
 
 export default definePlugin({
     name: "Emotions",
@@ -16,366 +80,5 @@ export default definePlugin({
     ],
     description: "Add commands to express your emotions.",
     dependencies: ["CommandsAPI"],
-    commands: [
-        {
-            name: "lurk",
-            description: "Send random lurk gif.",
-            execute: async opts => {
-                return {
-                    content: `[lurk](${await getImage('lurk')})`
-                };
-            }
-        },
-        {
-            name: "shoot",
-            description: "Send random shoot gif.",
-            execute: async opts => {
-                return {
-                    content: `[shoot](${await getImage('shoot')})`
-                };
-            }
-        },
-        {
-            name: "sleep",
-            description: "Send random sleep gif.",
-            execute: async opts => {
-                return {
-                    content: `[sleep](${await getImage('sleep')})`
-                };
-            }
-        },
-        {
-            name: "shrug-gif",
-            description: "Send random shrug gif.",
-            execute: async opts => {
-                return {
-                    content: `[shrug](${await getImage('shrug')})`
-                };
-            }
-        },
-        {
-            name: "stare",
-            description: "Send random stare gif.",
-            execute: async opts => {
-                return {
-                    content: `[stare](${await getImage('stare')})`
-                };
-            }
-        },
-        {
-            name: "wave",
-            description: "Send random wave gif.",
-            execute: async opts => {
-                return {
-                    content: `[wave](${await getImage('wave')})`
-                };
-            }
-        },
-        {
-            name: "poke",
-            description: "Send random poke gif.",
-            execute: async opts => {
-                return {
-                    content: `[poke](${await getImage('poke')})`
-                };
-            }
-        },
-        {
-            name: "smile",
-            description: "Send random smile gif.",
-            execute: async opts => {
-                return {
-                    content: `[smile](${await getImage('smile')})`
-                };
-            }
-        },
-        {
-            name: "peck",
-            description: "Send random peck gif.",
-            execute: async opts => {
-                return {
-                    content: `[peck](${await getImage('peck')})`
-                };
-            }
-        },
-        {
-            name: "wink",
-            description: "Send random wink gif.",
-            execute: async opts => {
-                return {
-                    content: `[wink](${await getImage('wink')})`
-                };
-            }
-        },
-        {
-            name: "blush-gif",
-            description: "Send random blush gif.",
-            execute: async opts => {
-                return {
-                    content: `[blush](${await getImage('blush')})`
-                };
-            }
-        },
-        {
-            name: "smug-gif",
-            description: "Send random smug gif.",
-            execute: async opts => {
-                return {
-                    content: `[smug](${await getImage('smug')})`
-                };
-            }
-        },
-        {
-            name: "tickle",
-            description: "Send random tickle gif.",
-            execute: async opts => {
-                return {
-                    content: `[tickle](${await getImage('tickle')})`
-                };
-            }
-        },
-        {
-            name: "yeet",
-            description: "Send random yeet gif.",
-            execute: async opts => {
-                return {
-                    content: `[yeet](${await getImage('yeet')})`
-                };
-            }
-        },
-        {
-            name: "think",
-            description: "Send random think gif.",
-            execute: async opts => {
-                return {
-                    content: `[think](${await getImage('think')})`
-                };
-            }
-        },
-        {
-            name: "highfive",
-            description: "Send random highfive gif.",
-            execute: async opts => {
-                return {
-                    content: `[highfive](${await getImage('highfive')})`
-                };
-            }
-        },
-        {
-            name: "feed",
-            description: "Send random feed gif.",
-            execute: async opts => {
-                return {
-                    content: `[feed](${await getImage('feed')})`
-                };
-            }
-        },
-        {
-            name: "bite",
-            description: "Send random bite gif.",
-            execute: async opts => {
-                return {
-                    content: `[bite](${await getImage('bite')})`
-                };
-            }
-        },
-        {
-            name: "bored",
-            description: "Send random bored gif.",
-            execute: async opts => {
-                return {
-                    content: `[bored](${await getImage('bored')})`
-                };
-            }
-        },
-        {
-            name: "nom",
-            description: "Send random nom gif.",
-            execute: async opts => {
-                return {
-                    content: `[nom](${await getImage('nom')})`
-                };
-            }
-        },
-        {
-            name: "yawn",
-            description: "Send random yawn gif.",
-            execute: async opts => {
-                return {
-                    content: `[yawn](${await getImage('yawn')})`
-                };
-            }
-        },
-        {
-            name: "facepalm",
-            description: "Send random facepalm gif.",
-            execute: async opts => {
-                return {
-                    content: `[facepalm](${await getImage('facepalm')})`
-                };
-            }
-        },
-        {
-            name: "cuddle",
-            description: "Send random cuddle gif.",
-            execute: async opts => {
-                return {
-                    content: `[cuddle](${await getImage('cuddle')})`
-                };
-            }
-        },
-        {
-            name: "kick-gif",
-            description: "Send random kick gif.",
-            execute: async opts => {
-                return {
-                    content: `[kick](${await getImage('kick')})`
-                };
-            }
-        },
-        {
-            name: "happy-gif",
-            description: "Send random happy gif.",
-            execute: async opts => {
-                return {
-                    content: `[happy](${await getImage('happy')})`
-                };
-            }
-        },
-        {
-            name: "hug",
-            description: "Send random hug gif.",
-            execute: async opts => {
-                return {
-                    content: `[hug](${await getImage('hug')})`
-                };
-            }
-        },
-        {
-            name: "baka",
-            description: "Send random baka gif.",
-            execute: async opts => {
-                return {
-                    content: `[baka](${await getImage('baka')})`
-                };
-            }
-        },
-        {
-            name: "pat",
-            description: "Send random pat gif.",
-            execute: async opts => {
-                return {
-                    content: `[pat](${await getImage('pat')})`
-                };
-            }
-        },
-        {
-            name: "nod",
-            description: "Send random nod gif.",
-            execute: async opts => {
-                return {
-                    content: `[nod](${await getImage('nod')})`
-                };
-            }
-        },
-        {
-            name: "nope",
-            description: "Send random nope gif.",
-            execute: async opts => {
-                return {
-                    content: `[nope](${await getImage('nope')})`
-                };
-            }
-        },
-        {
-            name: "kiss",
-            description: "Send random kiss gif.",
-            execute: async opts => {
-                return {
-                    content: `[kiss](${await getImage('kiss')})`
-                };
-            }
-        },
-        {
-            name: "dance",
-            description: "Send random dance gif.",
-            execute: async opts => {
-                return {
-                    content: `[dance](${await getImage('dance')})`
-                };
-            }
-        },
-        {
-            name: "punch",
-            description: "Send random punch gif.",
-            execute: async opts => {
-                return {
-                    content: `[punch](${await getImage('punch')})`
-                };
-            }
-        },
-        {
-            name: "handshake",
-            description: "Send random handshake gif.",
-            execute: async opts => {
-                return {
-                    content: `[handshake](${await getImage('handshake')})`
-                };
-            }
-        },
-        {
-            name: "slap",
-            description: "Send random slap gif.",
-            execute: async opts => {
-                return {
-                    content: `[slap](${await getImage('slap')})`
-                };
-            }
-        },
-        {
-            name: "cry",
-            description: "Send random cry gif.",
-            execute: async opts => {
-                return {
-                    content: `[cry](${await getImage('cry')})`
-                };
-            }
-        },
-        {
-            name: "pout",
-            description: "Send random pout gif.",
-            execute: async opts => {
-                return {
-                    content: `[pout](${await getImage('pout')})`
-                };
-            }
-        },
-        {
-            name: "handhold",
-            description: "Send random handhold gif.",
-            execute: async opts => {
-                return {
-                    content: `[handhold](${await getImage('handhold')})`
-                };
-            }
-        },
-        {
-            name: "thumbsup",
-            description: "Send random thumbsup gif.",
-            execute: async opts => {
-                return {
-                    content: `[thumbsup](${await getImage('thumbsup')})`
-                };
-            }
-        },
-        {
-            name: "laugh",
-            description: "Send random laugh gif.",
-            execute: async opts => {
-                return {
-                    content: `[laugh](${await getImage('laugh')})`
-                };
-            }
-        }
-    ]
+    commands
 });
